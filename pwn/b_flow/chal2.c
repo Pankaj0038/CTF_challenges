4a76b2d9c0620e956fce012bbe9aff8073c811a7
Sandipan Chatterjee <sandipan3002@gmail.com>
2024-02-04 15:01:20 +0530

    update

--- pwn/b_flow/chal2.c
+++ pwn/b_flow/chal2.c
@@ -18,7 +18,7 @@ void ret2win(){
 	printf(ANSI_COLOR_GREEN" \\___ \\  | |       / __ \\    \\   / "	 "\n");
 	printf(ANSI_COLOR_GREEN" ____) | | |____  / ____ \\    | |  "		 "\n");
 	printf(ANSI_COLOR_GREEN"|_____/  |______|/_/    \\_\\   |_|  "	 "\n");
-	printf(ANSI_COLOR_YELLOW"You need to bypass me to get the flag: "	 "\n");
+	printf(ANSI_COLOR_YELLOW"You need to overflow me to get the flag "	 "\n");
 	printf(ANSI_COLOR_RED"root" ANSI_COLOR_GREEN "@gengar:/" ANSI_COLOR_WHITE ">");
 	fflush(stdout);
 	gets(buffer);
